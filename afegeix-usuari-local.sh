3977b74399ef0d71117524cfd6380624eac3be9a
DJBeck-INSPedralbes <57413391+DJBeck-INSPedralbes@users.noreply.github.com>
2019-11-05 20:43:03 +0100

    Add files via upload

---
+++ afegeix-usuari-local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#if [[ "${UID}" -eq 0 ]]
+#then
+	read -p 'Enter the username to create: ' USER_NAME
+	read -p 'Enter the name of the person who this account is for: ' COMMENT
+	read -p 'Enter the password to use for the account: ' PASSWORD
+	useradd -c "${COMMENT}" -m ${USER_NAME}
+	echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+	passwd -e ${USER_NAME}
+#else
+#	echo 'You are not root'
+#	exit 1
+#fi
+
